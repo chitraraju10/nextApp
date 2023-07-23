ed91a51108b2601a9551361af829c14c314f58fe
unknown <chitraraju018@gmail.com>
2023-07-23 18:08:32 +0530

    fixed errors

--- components/posts/components/completePostPage/index.jsx
+++ components/posts/components/completePostPage/index.jsx
@@ -7,21 +7,16 @@ function CompletePost() {
 
     const data = viewReportData?.filter((p) => p?.id == query.id)
 
+    console.log('data:',data)
+
   	return (
 		<div className="m-auto prose p-10">
 			<div>
-				
-				{/* <p className="text-[#737373]">{data[0]?.published_date}</p> */}
 				<h1 className="text-[28px] font-semibold">
-					{data[0]?.title}
+					{data[0]?.title || 'Loading...'}
 				</h1>
-				{/* <div>
-					<p className="text-[#949494] text-sm  ">Written by</p>
-					<p className="text-[#3A3A3A]">{data[0]?.written_by}</p>
-				</div> */}
-                <p>{data[0]?.description}</p>
+                <p>{data[0]?.description || 'Loading...'}</p>
 			</div>
-			
 		</div>
 	);
 }
